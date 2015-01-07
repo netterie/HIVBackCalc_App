3a6c470b8b26a1253d06c8358c8a426359b50a2b
Jeanette Birnbaum <kurian@uw.edu>
2015-01-07 13:18:16 -0800

    File that sources dependent packages for app

---
+++ package_installs.R
@@ -0,0 +1,13 @@
+
+############################################################
+# INSTALL PACKAGE DEPENDENCIES FOR HIVBackCalc_App
+############################################################
+
+install.packages('shiny', type='source')
+install.packages('rootSolve')
+install.packages('ggplot2') 
+install.packages('reshape') 
+install.packages('Hmisc') 
+install.packages('scales') 
+install.packages('plyr') 
+install.packages('xtable') 
