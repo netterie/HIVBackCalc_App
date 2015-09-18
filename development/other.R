39b13c326d29e106b085233ee3de0997f6df331f
netterie <jeanettek@gmail.com>
2015-09-18 09:52:23 -0700

    Added tryCatch function to other.R

--- development/other.R
+++ development/other.R
@@ -871,3 +871,18 @@ trueprev_figure_4cases <- function(files,
 
     return(pl)
 } 
+
+
+tryCatch.W.E <- function(expr)
+{
+        W <- NULL
+    w.handler <- function(w){ # warning handler
+            W <<- w
+        invokeRestart("muffleWarning")
+            }
+        list(value = withCallingHandlers(tryCatch(expr, error = function(e) e),
+                                                             warning = w.handler),
+                 warning = W)
+}
+
+
